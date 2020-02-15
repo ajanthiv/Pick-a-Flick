35b3766a8321f6d973195891a7c2dfe3e3688ed0
Ajanthi Vasantharoopan <ajanthi.v@gmail.com>
2020-02-15 02:01:30 -0500

    font awesome icon library

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,5 +2,21 @@ const $ = jQuery
 const $document = $(document)
 
 $document.ready(() => {
-  console.log('hello')
+  $.ajax({
+    method: 'GET',
+    url: 'https://api.themoviedb.org/3/discover/movie',
+    data: {
+      api_key:'da62a5d685a5d7690a4f192f5400cdcb',
+      language: 'en-US',
+      sort_by: 'popularity.desc',
+      adult: false,
+      include_video: false,
+      page: 1,
+      primary_release_year: '2019'
+    },
+    success: (data) => {
+      console.log('data', data)
+    },
+    dataType: 'json'
+  })
 })
